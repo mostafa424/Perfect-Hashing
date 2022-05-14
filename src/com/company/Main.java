402f0ca93c9d1a9608fa1d15e0b5e1605823f42f
Omar Safa <safa.omar72@gmail.com>
2022-05-14 16:57:57 +0200

    fix minor bugs, support for negative keys

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> values = new ArrayList<>(List.of("bop", "beep", "meep", "mop", "cube", "hen", "kappa", "pogchamp"));
+        List<Integer> keys = new ArrayList<>(List.of(2, 15, 21, 39, 815, 1, 210, 399));
+        MultilevelStaticTable<String> hashTable = new MultilevelStaticTable<>(10, keys, values);
     }
 }
--- src/com/company/NSquarePerfectHasher.java
+++ src/com/company/NSquarePerfectHasher.java
@@ -1,69 +1,82 @@
 package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Perfect Hashing in O(N2) space complexity
- * table: Hash table  containg values stored after hashing (Immutable)
- * keys: array of keys to be hashed(assume keys are 8-bits long)
- * values: array of values to be stored
+ * table: Hash table containing values stored after hashing (Immutable)
  * hashEngine: class used for universal hashing
- * collisionRate: How many times it took to recompute the hashfunction before storing values in the table
+ * collisionRate: How many times it took to recompute the hash function before storing values in the table
  **/
-public class NSquarePerfectHasher<T> implements PerfectHasher{
-    private  Object []table;
-    private  Object []values;
-    private  int[]keys;
+public class NSquarePerfectHasher<T> {
+    private ArrayList<T> table;
     private Hasher hashEngine;
-
     private int collisionRate;
-
-
 
     /**
      * Constructor
      */
-    public NSquarePerfectHasher(Object[] values,int[]keys) {
-        this.table = new Object [(int) Math.pow(values.length, 2)];
-        this.values=values;
-        this.keys=keys;
-        this.hashEngine = new MatrixHasher((byte)(Math.log(values.length)/Math.log(2)),(byte) 8);
-        perfectHash();
+    public NSquarePerfectHasher(int keyBitsNum, List<Integer> keys, List<T> values) {
+        int size;
+        if(values.size() != 0) size = 1<<(int)Math.ceil(Math.log(values.size())/Math.log(2));
+        else return;
+        this.table = new ArrayList<>(size * size);
+        this.hashEngine = new MatrixHasher((int)(2*Math.log(size)/Math.log(2)), keyBitsNum);
+        initTable(size * size);
+        perfectHash(keys, values);
     }
+
     /**
-    * used to hash keys and store elements into the hashtable
-     * if some keys have the same hashvalue a hashEngine is generated with a new hash function
+     * Method to initialize table to required size.
+     *
+     * @param size size to initialize table to.
+     */
+    private void initTable(int size) {
+        for(int i = 0; i < size; i++) {
+            this.table.add(null);
+        }
+    }
+
+    /**
+     * used to hash keys and store elements into the hashtable
+     * if some keys have the same hash value a hashEngine is generated with a new hash function
      * before storing in the table
     **/
-    @Override
-    public void perfectHash() {
-        int[]hashTable=getHashTable(keys);
-        while(check_for_collision(hashTable)){
-            hashEngine=new MatrixHasher((byte)(Math.log(values.length)/Math.log(2)),(byte) 8);
-            hashTable=getHashTable(keys);
+    private void perfectHash(List<Integer> keys, List<T> values) {
+        List<Integer> hashTable = getHashTable(keys);
+        while(check_for_collision(hashTable)) {
+            hashTable = getHashTable(keys);
         }
-        int k=0;
-        for(int i : keys){
-            table[hashTable[i]]=values[k++];
+        for(int i = 0; i < keys.size(); i++){
+            table.set(hashTable.get(i), values.get(i));
         }
     }
-    /**used to hash keys
+
+    /**
+     * used to hash keys
      * @param keys :array containing keys to be hashed
-     * @return array containg hashed value of keys
+     * @return array containing hashed value of keys
      **/
-    private int[] getHashTable(int[] keys){
-        int[]hashTable=new int[keys.length];
-        for(int i=0;i< keys.length;i++){
-            hashTable[i]=hashEngine.hash(keys[i]);
+    private List<Integer> getHashTable(List<Integer> keys){
+        List<Integer> hashTable = new ArrayList<>(keys.size());
+        this.hashEngine.generateFunction();
+        for (Integer key : keys) {
+            hashTable.add(hashEngine.hash(key));
         }
         return hashTable;
     }
-    /**checks if any of the keys have the same hashvalue before storing them in the table
+
+    /**
+     * checks if any of the keys have the same hash value before storing them in the table
      * for recomputing the hash function
-     * @param hashtable :array containg hashed value of keys
+     * @param hashTable :array containing hashed value of keys
      * @return boolean indicates if there is collision or not
      */
-    private boolean check_for_collision(int[]hashtable){
-        for(int i=0;i<hashtable.length;i++){
-            for(int j=i+1;j<hashtable.length;j++){
-                if(hashtable[i]==hashtable[j]){
+    private boolean check_for_collision(List<Integer> hashTable){
+        for(int i = 0; i < hashTable.size(); i++){
+            for(int j = i+1; j < hashTable.size(); j++){
+                if(hashTable.get(i).equals(hashTable.get(j))){
                     collisionRate++;
                     return true;
                 }
@@ -71,7 +84,16 @@ public class NSquarePerfectHasher<T> implements PerfectHasher{
         }
         return false;
     }
+
     public int getCollisionRate() {
         return collisionRate;
+    }
+
+    public T get(int key) {
+        return this.table.get(this.hashEngine.hash(key));
+    }
+
+    public void update(int key, T val) {
+        this.table.set(this.hashEngine.hash(key), val);
     }
 }
